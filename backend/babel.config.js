571685bc652733a19b50c2268be02f4f39521c1a
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2021-06-27 16:42:00 -0500

    CHECKPOINT: start backend file structure

---
+++ backend/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    'dynamic-import-node'],
+};
---
+++ backend/server.js
@@ -0,0 +1,4 @@
+
+import _get from 'lodash/get';
+
+console.log(_get({ hola: 'mundo' }, 'hola'));
