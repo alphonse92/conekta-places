9cf22dc0904f7f78aea75859e388691f0c87b606
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2021-06-27 19:58:06 -0500

    CHECKPOINT: routes and controllers created for address crud

--- backend/src/v1/controllers/address.controller.js
+++ backend/src/v1/controllers/address.controller.js
@@ -1,5 +1,26 @@
+export const getAddress = (req, res) => {
+  // Do something to store  in database
+  res.send('ok');
+};
+
+export const getAddresses = (req, res) => {
+  // Do something to store  in database
+  res.send('ok');
+};
+
 export const saveAddress = (req, res) => {
   const { body } = req;
   // Do something to store  in database
   res.send(body);
+};
+
+export const updateAddress = (req, res) => {
+  const { body } = req;
+  // Do something to store  in database
+  res.send(body);
+};
+
+export const deleteAddress = (req, res) => {
+  // Do something to store  in database
+  res.send('ok');
 };
--- backend/src/v1/routes/address.js
+++ backend/src/v1/routes/address.js
@@ -1,8 +1,18 @@
 import { Router as expresRouter } from 'express';
-import { saveAddress } from '../controllers/address.controller';
+import {
+  deleteAddress,
+  getAddress,
+  getAddresses,
+  saveAddress,
+  updateAddress,
+} from '../controllers/address.controller';
 
 const router = expresRouter();
 
+router.get('/address/', getAddress);
+router.get('/address/:id', getAddresses);
 router.post('/address/', saveAddress);
+router.put('/address/', updateAddress);
+router.delete('/address/:id', deleteAddress);
 
 export default router;
